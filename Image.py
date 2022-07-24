6d3aa814061bce034346d299d0a6cccc5338eee8
Chan Lee <chan1122@g.skku.edu>
2022-07-25 08:34:58 +0900

    Base structure of the image processing module is now functional

--- Image.py
+++ Image.py
@@ -1,6 +1,6 @@
 #Built-in Modules *************************************************************
 
-import os, pygame, time
+import os, pygame, sys, time
 
 
 
@@ -10,13 +10,41 @@ import Data
 
 
 
+#Classes **********************************************************************
+
+class vertex:
+    def __init__(self, prop: str) -> None:
+        self.prop = prop
+
+
+
+#Variables ********************************************************************
+
+default_image = pygame.image.load(Data.resource_path("images\default_image.png")).convert_alpha()
+
+
+
+#Class Variables **************************************************************
+
+top_left = vertex("top left")
+top_mid = vertex("top middle")
+top_right = vertex("top right")
+mid_left = vertex("middle left")
+center = vertex("center")
+mid_right = vertex("middle right")
+bot_left = vertex("bottom left")
+bot_mid = vertex("bottom middle")
+bot_right = vertex("bottom right")
+
+
+
 #Functions ********************************************************************
 
 def load(path: str) -> None:
     try: return pygame.image.load(Data.resource_path(path)).convert_alpha()
-    except: return pygame.image.load(Data.resource_path("images\default_image.png")).convert_alpha()
+    except: return default_image
 
-def screenshot(image: pygame.Surface, path: str) -> None:
+def screenshot(path: str) -> None:
     main_path = Data.game_folder_path()
     if os.path.isdir(main_path):
         screenshot_folder_path = main_path + "\screenshots"
@@ -32,11 +60,42 @@ def screenshot(image: pygame.Surface, path: str) -> None:
         for i in range(2, 2147483647):
             if not os.path.isfile(current_screenshot_name): break
             current_screenshot_name = "screenshot_%s-%s-%s-%s-%s-%s(%d).png" % (year, month, day, hour, minute, second, i)
-        pygame.image.save(image, screenshot_folder_path + "\\" + current_screenshot_name)
+        pygame.image.save(SCREEN, screenshot_folder_path + "\\" + current_screenshot_name)
         return True
     else:
         Data.game_folder_recover()
         return False
 
-def scale(image: pygame.Surface, target_size: (int, int)) -> pygame.Surface:
-    return pygame.transform.smoothscale(image, target_size)
+def scale(image: pygame.Surface, target_scale: int or float or (int, int) = 1) -> pygame.Surface:
+    if type(target_scale) in (int, float):
+        size = image.get_rect().size
+        new_size = tuple(map(int, (size[0] * target_scale, size[1] * target_scale)))
+        return pygame.transform.smoothscale(image, new_size)
+    else:
+        try:
+            return pygame.transform.smoothscale(image, target_scale)
+        except:
+            raise TypeError("scale() expected one of int, float, (int, int), but %s found" % str(type(target_scale)).split('\'')[1])
+
+def draw(screen: pygame.Surface, image: pygame.Surface, pos: (int, int) = (0, 0), vertex: vertex = top_left) -> None:
+    rect = image.get_rect()
+    if vertex == top_mid:
+        rect.midtop = pos
+    elif vertex == top_right:
+        rect.topright = pos
+    elif vertex == mid_left:
+        rect.midleft = pos
+    elif vertex == center:
+        rect.center = pos
+    elif vertex == mid_right:
+        rect.midright = pos
+    elif vertex == bot_left:
+        rect.bottomleft = pos
+    elif vertex == bot_mid:
+        rect.midbottom = pos
+    elif vertex == bot_right:
+        rect.bottomright = pos
+    else: #default: vertex == top_left
+        rect.topleft = pos
+
+    screen.blit(image, rect)
