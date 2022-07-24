c657cbd78f222e540dbd8ae7f9e3872618ea7dc5
Chan Lee <chan1122@g.skku.edu>
2022-07-25 08:35:52 +0900

    The base structure of the GUI text processing module is now functional

--- Text.py
+++ Text.py
@@ -12,7 +12,7 @@ import Data, Image
 
 #Classes **********************************************************************
 
-class pos:
+class align:
     def __init__(self, prop: str) -> None:
         self.prop = prop
 
@@ -33,33 +33,23 @@ except:
 
 #Class Variables **************************************************************
 
-top_left = pos("top left")
-top_mid = pos("top middle")
-top_right = pos("top right")
-mid_left = pos("middle left")
-center = pos("center")
-mid_right = pos("middle right")
-bot_left = pos("bottom left")
-bot_mid = pos("bottom middle")
-bot_right = pos("bottom right")
-
-left = pos("left")
-middle = pos("middle")
-right = pos("right")
+left = align("left")
+middle = align("middle")
+right = align("right")
 
 
 
 #Functions ********************************************************************
 
-def draw_text(screen: pygame.Surface, content: str = "", pos: (int, int) = (0, 0), font_size: int = 16, origin: str = top_left, align: str = left) -> None:
+def text_to_image(content: str, font_size: int = 16, align: align = left) -> pygame.Surface:
     line = content.split('\n')
     longest = max(len(i) for i in line)
-    field_width = font_size * longest
-    field_height = font_size * len(line)
-    base = pygame.Surface((field_width, field_height), pygame.SRCALPHA)
     character_width = font_size * 14 // 16
     character_height = font_size
-    
+    field_width = character_width * longest
+    field_height = character_height * len(line)
+    base = pygame.Surface((field_width, field_height), pygame.SRCALPHA)
+
     cursor = [0, -font_size]
     for i in line:
         cursor[0] = character_width * (longest - len(i)) * (1 if align == middle else 2 if align == right else 0) // 2
@@ -71,30 +61,12 @@ def draw_text(screen: pygame.Surface, content: str = "", pos: (int, int) = (0, 0
             try:
                 original_character = font_image.subsurface((horizontal, vertical, 14, 16))
             except:
-                original_character = font_image.subsurface((0, 0, 14, 16))
-            transformed_character = pygame.transform.scale(original_character, (character_width, character_height))
-            base.blit(transformed_character, cursor)
+                original_character = font_image.subsurface((1, 0, 14, 16))
+            transformed_character = Image.scale(original_character, (character_width, character_height))
+            Image.draw(base, transformed_character, cursor)
             cursor[0] += character_width
+    return base
 
-    text_field = base.get_rect()
-    if origin == top_mid:
-        text_field.midtop = pos
-    elif origin == top_right:
-        text_field.topright = pos
-    elif origin == mid_left:
-        text_field.midleft = pos
-    elif origin == center:
-        text_field.centerx = pos[0]
-        text_field.centery = pos[1]
-    elif origin == right:
-        text_field.midright = pos
-    elif origin == bot_left:
-        text_field.bottomleft = pos
-    elif origin == bot_mid:
-        text_field.midbottom = pos
-    elif origin == bot_right:
-        text_field.bottomright = pos
-    else: #default: origin == top_left
-        text_field.topleft = pos
-
-    screen.blit(base, text_field)
+def draw_text(screen: pygame.display, content: str, font_size: int = 16, align: align = left, pos: (int, int) = (0, 0), vertex: Image.vertex = Image.top_left) -> None:
+    text_image = text_to_image(content, font_size, align)
+    Image.draw(screen, text_image, pos, vertex)
