4d4e3648a23adbb396a97c0be8662c7ad0e751cd
Chan Lee <chan1122@g.skku.edu>
2022-08-05 01:40:59 +0900

    Initialization method has been changed a little bit

--- Event.py
+++ Event.py
@@ -14,15 +14,28 @@ import Data, Image, Text
 
 
 
-#Initialization Functions *****************************************************
+#Initialization ***************************************************************
 
-main = sys.modules["__main__"]
+if __name__ != "__main__":
+    main = sys.modules["__main__"]
+else:
+    raise NameError("")
 try:
     SCREEN = main.SCREEN
 except:
     raise NameError("To use 'Event' module, '__main__' module must have initialized 'pygame.display' object named 'SCREEN'")
-RESOLUTION = main.RESOLUTION
-FPS = main.FPS
+try:
+    RESOLUTION = main.RESOLUTION
+except:
+    raise NameError("To use 'Event' module, '__main__' module must have initialized '(int, int)' object named 'RESOLUTION'")
+try:
+    FPS = main.FPS
+except:
+    raise NameError("To use 'Event' module, '__main__' module must have initialized 'int' object named 'FPS'")
+try:
+    MAGNIFYING_RATE = main.MAGNIFYING_RATE
+except:
+    raise NameError("To use 'Event' module, '__main__' module must have initialized 'int' or 'float' object named 'MAGNIFYING_RATE'")
 
 
 
@@ -39,7 +52,7 @@ class status:
         self.name = name
         self.dict = {}
         self.event = []
-        self.init = lambda: None
+        self.init = functools.partial(lambda e: None, self)
 
     def set_init_function(self, func: "function") -> None:
         self.init = functools.partial(func, self)
@@ -66,24 +79,24 @@ class status:
 
 
 
-#Sample Conditions ************************************************************
+#Sample Conditions ****************************************
 always = lambda s: True
 never = lambda s: False
 
-#Sample Actions ***************************************************************
+#Sample Actions *******************************************
 idling = lambda s: None
 
 
 
 #Statuses *********************************************************************
 
-#Undefined ********************************************************************
+#Undefined ************************************************
 undefined = status("undefined")
 undefined_event = event("undefined_event", always, idling)
 undefined.set_init_function(lambda u: u.event_append(undefined_event))
 undefined.init()
 
-#Starting *********************************************************************
+#Starting *************************************************
 starting = status("starting")
 
 def starting_init(st: status) -> None:
@@ -95,16 +108,16 @@ def starting_background_action(st: status) -> None:
     base.fill((0, 0, 0))
     
     logo = Image.load("images\logo.png")
-    logo = Image.scale(logo, 4)
-
     Image.draw(base, logo, base.get_rect().center, Image.center)
+
+    Image.scale(base, MAGNIFYING_RATE)
     Image.draw(SCREEN, base)
 
 def starting_fade_in_action(st: status) -> None:
     if "fade_in_counter" not in st.dict:
-        st.dict_append("fade_in_counter", FPS*2)
+        st.dict_append("fade_in_counter", FPS*1.5)
 
-    cur_alpha = 255 * st.dict["fade_in_counter"] // (FPS*2)
+    cur_alpha = 255 * st.dict["fade_in_counter"] // (FPS*1.5)
 
     curtain = pygame.Surface(SCREEN.get_size(), pygame.SRCALPHA)
     curtain.fill((0, 0, 0))
@@ -121,7 +134,7 @@ def starting_fade_in_action(st: status) -> None:
 
 def starting_wait_action(st: status) -> None:
     if "wait_counter" not in st.dict:
-        st.dict_append("wait_counter", FPS*2)
+        st.dict_append("wait_counter", FPS)
 
     st.dict["wait_counter"] -= 1
 
@@ -132,9 +145,9 @@ def starting_wait_action(st: status) -> None:
 
 def starting_fade_out_action(st: status) -> None:
     if "fade_out_counter" not in st.dict:
-        st.dict_append("fade_out_counter", FPS*2)
+        st.dict_append("fade_out_counter", FPS*1.5)
 
-    cur_alpha = 255 - 255 * st.dict["fade_out_counter"] // (FPS*2)
+    cur_alpha = 255 - 255 * st.dict["fade_out_counter"] // (FPS*1.5)
 
     curtain = pygame.Surface(SCREEN.get_size(), pygame.SRCALPHA)
     curtain.fill((0, 0, 0))
@@ -146,7 +159,8 @@ def starting_fade_out_action(st: status) -> None:
 
     if st.dict["fade_out_counter"] < 0:
         st.dict_del("fade_out_counter")
-        main.GAME_STATUS = undefined
+        main.GAME_STATUS = title_screen
+        title_screen.init()
 
 starting_background_event = event("starting_background_event", always, starting_background_action)
 starting_fade_in_event = event("starting_fade_in_event", always, starting_fade_in_action)
@@ -156,14 +170,23 @@ starting_fade_out_event = event("starting_fade_out_event", always, starting_fade
 starting.set_init_function(starting_init)
 starting.init()
 
+#Title Screen *********************************************
+title_screen = status("title_screen")
 
+def title_screen_init(st: status):
+    st.event_append(title_screen_background_event)
 
+def title_screen_background_action(st: status) -> None:
+    SCREEN.fill((40, 40, 120)) #Will be title_screen_image(.png) later...
 
+    title_screen_board = Image.load("images\classic_board.png")
 
+    Image.scale(title_screen_board, MAGNIFYING_RATE)
+    Image.draw(SCREEN, title_screen_board, pos = (160, 213))
 
-#Class Variables **************************************************************
+    Text.draw_text(SCREEN, "AI Gameplay\nwill be\nhere.", font_size = 16 * MAGNIFYING_RATE, align = Text.middle, pos = (240, 405), vertex = Image.center, color = (255, 255, 255)) #Temporary Test Code
 
-#Events ***********************************************************************
+title_screen_background_event = event("title_screen_background_event", always, title_screen_background_action)
 
-
-#Statuses *********************************************************************
+title_screen.set_init_function(title_screen_init)
+title_screen.init()
